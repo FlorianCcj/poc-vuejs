e990b26d49dd5cf3a038df6acd0f99dd921ab7d0
florianccj <florianccj@gmail.com>
2018-07-07 08:45:28 +0200

    07 confition ok

--- 00-essential/06-class-and-style-bindings/index.js
+++ 00-essential/06-class-and-style-bindings/index.js
@@ -28,10 +28,10 @@ new Vue({
   el: '#style',
   data: {
     activeColor: 'red',
-  fontSize: 30,
-  styleObject: {
-    color: 'red',
-    fontSize: '13px'
-  }
+    fontSize: 30,
+    styleObject: {
+      color: 'red',
+      fontSize: '13px'
+    }
   }
 })
---
+++ 00-essential/07-conditional-rendering/index.js
@@ -0,0 +1,9 @@
+new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue.js!',
+    type: 'A',
+    ok: true,
+    loginType: 'username'
+  }
+})
