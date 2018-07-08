6d74fb0fabe52d381546fb28c064964a965f86be
florianccj <florianccj@gmail.com>
2018-07-08 14:34:37 +0200

    component

---
+++ 01-components-in-depth/01-component_registration/ComponentB.js
@@ -0,0 +1,10 @@
+import ComponentA from './ComponentA'
+import ComponentC from './ComponentC'
+
+export default {
+  components: {
+    ComponentA,
+    ComponentC
+  },
+  // ...
+}
--- 01-components-in-depth/01-component_registration/index.js
+++ 01-components-in-depth/01-component_registration/index.js
@@ -1,6 +1,78 @@
 new Vue({
-  el: '#app',
+  el: '#app1',
   data: {
     message: 'Hello Vue.js!'
+  }
+})
+
+// global
+
+Vue.component('component-a', { /* ... */ })
+Vue.component('component-b', { /* ... */ })
+Vue.component('component-c', { /* ... */ })
+
+new Vue({ el: '#app' })
+
+// end global
+// local
+
+var ComponentA = { /* ... */ }
+var ComponentB = { /* ... */ }
+var ComponentC = { /* ... */ }
+
+new Vue({
+  el: '#app',
+  components: {
+    'component-a': ComponentA,
+    'component-b': ComponentB
+  }
+})
+
+// to use comp1 in compB
+
+
+var ComponentB = {
+  components: {
+    'component-a': ComponentA
+  },
+  // ...
+}
+
+// end local
+
+// validation
+
+Vue.component('my-component', {
+  props: {
+    // Basic type check (`null` matches any type)
+    propA: Number,
+    // Multiple possible types
+    propB: [String, Number],
+    // Required string
+    propC: {
+      type: String,
+      required: true
+    },
+    // Number with a default value
+    propD: {
+      type: Number,
+      default: 100
+    },
+    // Object with a default value
+    propE: {
+      type: Object,
+      // Object or array defaults must be returned from
+      // a factory function
+      default: function () {
+        return { message: 'hello' }
+      }
+    },
+    // Custom validator function
+    propF: {
+      validator: function (value) {
+        // The value must match one of these strings
+        return ['success', 'warning', 'danger'].indexOf(value) !== -1
+      }
+    }
   }
 })
